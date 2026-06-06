5fd581c254cd881da4d4359dd8f1e7fcb0115b31
aleshaseliverstov00-jpg <alesha_seliverstov00@mail.ru>
2026-06-06 22:17:21 +0500

    исправление ошибок

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("Алексей: ")
-last_name = input("Селиверстов: ")
-print("Вас зовут:", last_name, first_name)
+first_name = input("")
+last_name = input("")
+print("Вас зовут", last_name, first_name)
