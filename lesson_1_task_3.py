c4abe36b9704b926d0740ceb0b2bd8b1c3b1b1f1
aleshaseliverstov00-jpg <alesha_seliverstov00@mail.ru>
2026-06-07 18:08:44 +0500

    Изменение в третьем задании

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("")
-last_name = input("")
-print("Вас зовут", last_name, first_name)
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
+print("Вас зовут:", last_name, first_name)
